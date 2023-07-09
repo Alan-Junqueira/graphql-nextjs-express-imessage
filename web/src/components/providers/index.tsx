d036be49fe184f4195c217d52e9862014be6b08a
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-07-09 12:28:05 -0300

    feat: Setup apollo client.

--- web/src/components/providers/index.tsx
+++ web/src/components/providers/index.tsx
@@ -4,6 +4,8 @@ import { ReactNode } from "react"
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from "@/chakra/theme"
+import { ApolloProvider } from "@apollo/client"
+import { client } from "@/graphql/apolo-client"
 
 interface IProviders {
   children: ReactNode
@@ -13,10 +15,12 @@ export const Providers = ({
   children
 }: IProviders) => {
   return (
-    <CacheProvider>
-      <ChakraProvider theme={theme}>
-        {children}
-      </ChakraProvider>
-    </CacheProvider>
+    <ApolloProvider client={client}>
+      <CacheProvider>
+        <ChakraProvider theme={theme}>
+          {children}
+        </ChakraProvider>
+      </CacheProvider>
+    </ApolloProvider>
   )
 }
