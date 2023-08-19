47df012923210eaa1ea6cd55c55ab9ac4eac10c3
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-08-19 15:08:52 -0300

    Model database backend, with prisma schema

--- api/src/graphql/resolvers/conversation.ts
+++ api/src/graphql/resolvers/conversation.ts
@@ -1,5 +1,17 @@
+import { GraphQlContext } from "../../@types/types";
+
 export const conversationResolvers = {
   Mutation: {
-    createConversation: async () => {}
-  }
-}
+    createConversation: async (
+      _parent: any,
+      args: { participantIds: Array<string> },
+      context: GraphQlContext,
+      _info: any
+    ) => {
+      const { prisma } = context;
+      const {participantIds} = args
+
+      console.log(participantIds)
+    },
+  },
+};
