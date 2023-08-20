c5f3cc9ece1df30fe6ce451d63cce9c2ffd38c91
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-08-20 12:22:53 -0300

    feat: Create prisma function and validator, to create a new conversation

--- api/src/graphql/resolvers/conversation.ts
+++ api/src/graphql/resolvers/conversation.ts
@@ -1,4 +1,6 @@
+import { ApolloError } from "apollo-server-core";
 import { GraphQlContext } from "../../@types/types";
+import { Prisma } from "@prisma/client";
 
 export const conversationResolvers = {
   Mutation: {
@@ -8,10 +10,61 @@ export const conversationResolvers = {
       context: GraphQlContext,
       _info: any
     ) => {
-      const { prisma } = context;
-      const {participantIds} = args
+      const { prisma /*session */ } = context;
+      const { participantIds } = args;
 
-      console.log(participantIds)
+      // if(!session.user){
+      //   throw new ApolloError("Not Authorized!")
+      // }
+
+      const userId = participantIds[0];
+      console.log(participantIds);
+
+      try {
+        const conversation = await prisma.conversation.create({
+          data: {
+            participants: {
+              createMany: {
+                data: participantIds.map((id) => ({
+                  userId: id,
+                  hasSeenLatestMessage: id === userId,
+                })),
+              },
+            },
+          },
+          include: conversationPopulated,
+        });
+      } catch (error) {
+        console.log("createConversation error", error);
+        throw new ApolloError("Error creating conversation");
+      }
     },
   },
 };
+
+export const participantPopulated =
+  Prisma.validator<Prisma.ConversationParticipantInclude>()({
+    user: {
+      select: {
+        id: true,
+        username: true,
+      },
+    },
+  });
+
+export const conversationPopulated =
+  Prisma.validator<Prisma.ConversationInclude>()({
+    participants: {
+      include: participantPopulated,
+    },
+    latestMessage: {
+      include: {
+        sender: {
+          select: {
+            id: true,
+            username: true,
+          },
+        },
+      },
+    },
+  });
